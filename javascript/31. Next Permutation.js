054a5a209f4f1246d3b66a6dead057ca9ee47757
SeanZhenggg <s410585038@gmail.com>
2022-06-20 10:47:08 +0800

    refactor(next Permutation): refactor code

--- javascript/31. Next Permutation.js
+++ javascript/31. Next Permutation.js
@@ -4,26 +4,30 @@
  */
 
 // reference answer from Vivian Liao
- var nextPermutation = function(nums) {
-  let successor = nums.length - 1, pivot = -1
-  
-  for(let i = nums.length - 1; i >= 0; i--) {
-      if(nums[i] > nums[i - 1]) {
-          pivot = i - 1
-          while(successor > pivot && nums[successor] <= nums[pivot]) {
-              successor -= 1
-          }
-          break
-      }
-  }
-
-  if(pivot !== -1 || successor !== nums.length - 1) {
-      [nums[successor], nums[pivot]] = [nums[pivot], nums[successor]]
-  }
-  for(let i = pivot + 1, j = nums.length - 1; i < nums.length; i++) {
-      if(i < j) {
-          [nums[i], nums[j]] = [nums[j], nums[i]]
-          j --;
-      }
-  }
+var nextPermutation = function (nums) {
+    let successor = nums.length - 1, pivot = -1
+    // find the pivot and successor
+    for (let i = nums.length - 1; i >= 0; i--) {
+        // pivot condition: nums[i] > nums[i - 1]
+        if (nums[i] > nums[i - 1]) {
+            pivot = i - 1
+            // successor condition: for nums[successor] > nums[pivot]
+            while (nums[successor] <= nums[pivot]) {
+                successor -= 1
+            }
+            break
+        }
+    }
+    // swap pivot and successor element
+    if (pivot !== -1 || successor !== nums.length - 1) {
+        [nums[successor], nums[pivot]] = [nums[pivot], nums[successor]]
+    }
+    // from pivot + 1 to end, reverse the rest elements
+    // cannot use slice(), reverse(), concat() cuz we need to do it in place
+    for (let i = pivot + 1, j = nums.length - 1; i < nums.length; i++) {
+        if (i < j) {
+            [nums[i], nums[j]] = [nums[j], nums[i]]
+            j--;
+        }
+    }
 };

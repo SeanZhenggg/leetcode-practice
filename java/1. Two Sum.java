e38d5fcdd0623e50a12e8f441e47707784df2151
Sean Zhengg <s410585038@gmail.com>
2023-01-22 14:07:59 +0800

    1. Two Sum using java

---
+++ java/1. Two Sum.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int[] result = new int[2];
+        HashMap<Integer, Integer> hashmap = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if(hashmap.containsKey(nums[i]) && hashmap.get(nums[i]) != i) {
+                result[0] = hashmap.get(nums[i]);
+                result[1] = i;
+                break;
+            } else {
+                hashmap.put(target - nums[i], i);
+            }
+        }
+        return result;
+    }
+}

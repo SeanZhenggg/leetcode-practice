6113a734771685baa29c05462ad713a8380bc40e
Sean Zhengg <s410585038@gmail.com>
2023-03-11 16:13:56 +0800

    update 217. Contains Duplicate.java

--- java/217. Contains Duplicate.java
+++ java/217. Contains Duplicate.java
@@ -1,3 +1,4 @@
+// HashMap solution
 class Solution {
     public boolean containsDuplicate(int[] nums) {
         Map<Integer, Integer> map = new HashMap<>();
@@ -5,6 +6,29 @@ class Solution {
             if (map.containsKey(nums[i])) return true;
             map.put(nums[i], 1);
         }
+
+        return false;
+    }
+}
+
+// sort solution
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Arrays.sort(nums);
+        for(int i = 1; i < nums.length; i++) {
+            if(nums[i] == nums[i - 1]) return true;
+        }
+
+        return false;
+    }
+}
+
+// HashSet solution
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for(int i = 0; i < nums.length; i++)
+            if(!set.add(nums[i])) return true;
 
         return false;
     }

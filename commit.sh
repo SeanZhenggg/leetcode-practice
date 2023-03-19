aaae586053b00beabea66f19ecc1b78bcc2eb00d
FE3-Sean <sean@innotech.me>
2023-03-19 13:44:10 +0800

    update commit shell script

--- commit.sh
+++ commit.sh
@@ -1,9 +1,19 @@
-git add :
+#!/bin/bash
 
-name=$(git status -s | sed -nE 's/.*([0-9]+.*\.).*/\1/p')
+# get mode (update or add)
+mode=$1
+
+# get filename and file extension
+name=$(git status -s | sed -nE 's/.*([0-9]+.*)\..*/\1/p')
 extension=$(git status -s | sed -nE 's/.*\.([a-z]+)"/\1/p')
 
-msg=$(echo $name | cut -c 1-$(($(echo $name | wc -c)-2)))
-git cm -m "$msg in $extension"
+if [[ mode == "update" ]]
+then
+  git add :
+  git cm -m "$name in $extension"
+else
+  git add :
+  git cm -m "update $name"
+fi
 
 git push origin master

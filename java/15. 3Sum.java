06aa634727a5b4d64348fedc60a022fd9ba6f785
FE3-Sean <sean@innotech.me>
2023-03-19 18:36:00 +0800

    15. 3Sum in java

---
+++ java/15. 3Sum.java
@@ -0,0 +1,29 @@
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        Arrays.sort(nums);
+
+        for(int i = 0; i <= nums.length - 3; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+            if(nums[i] > 0) break;
+
+            int l = i + 1, r = nums.length - 1;
+            while (l < r) {
+                if ((l > i + 1 && nums[l] == nums[l - 1]) && (r < nums.length - 1 && nums[r] == nums[r + 1])) {
+                    l++;
+                    r--;
+                    continue;
+                }
+
+                if (nums[i] + nums[l] + nums[r] == 0) {
+                    result.add(new ArrayList<Integer>(Arrays.asList(nums[i], nums[l], nums[r])));
+                    l++;
+                    r--;
+                }
+                else if (nums[i] + nums[l] + nums[r] > 0) r--;
+                else l++;
+            }
+        }
+        return result;
+    }
+}

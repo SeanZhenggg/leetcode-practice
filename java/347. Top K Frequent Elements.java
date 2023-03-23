4d0be1a1bb765aca9553fd6b5384cd15721aa95e
Sean Zhengg <s410585038@gmail.com>
2023-03-24 01:57:37 +0800

    update 347. Top K Frequent Elements

--- java/347. Top K Frequent Elements.java
+++ java/347. Top K Frequent Elements.java
@@ -29,6 +29,7 @@ class Node implements Comparable<Node> {
     }
 }
 
+// priority queue solution
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
         if(k == 1 && nums.length == 1) return nums;
@@ -51,6 +52,41 @@ class Solution {
             i++;
         }
         
+        return result;
+    }
+}
+
+// bucket sort solution
+class Solution {
+    public static int[] topKFrequent(int[] nums, int k) {
+        if (k == 1 && nums.length == 1) return nums;
+
+        Map<Integer, Integer> map = new HashMap<>();
+        List<Integer> buckets[] = new ArrayList[nums.length + 1];
+
+        for (int n : nums) {
+            map.merge(n, 1, Integer::sum);
+        }
+
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            int freq = entry.getValue();
+            if(buckets[freq] == null) {
+                buckets[freq] = new ArrayList<>();
+            }
+            buckets[freq].add(entry.getKey());
+        }
+
+        int j = 0;
+        int[] result = new int[k];
+        for(int i = nums.length; i >= 0; i--) {
+            if(buckets[i] != null) {
+                for(int n : buckets[i]) {
+                    result[j++] = n;
+                    if(j == k) break;
+                }
+                if(j == k) break;
+            }
+        }
         return result;
     }
 }

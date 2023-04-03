2aa8faef3ddac939bd979f4e31ba1a7e1e2cea17
Sean Zhengg <s410585038@gmail.com>
2023-04-04 01:16:58 +0800

    update 15. 3Sum

--- java/15. 3Sum.java
+++ java/15. 3Sum.java
@@ -27,3 +27,31 @@ class Solution {
         return result;
     }
 }
+
+// better solution
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        Arrays.sort(nums);
+
+        for(int i = 0; i <= nums.length - 3; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+            if(nums[i] > 0) break;
+
+            int l = i + 1, r = nums.length - 1;
+            while (l < r) {
+                if (nums[i] + nums[l] + nums[r] == 0) {
+                    result.add(new ArrayList<Integer>(Arrays.asList(nums[i], nums[l], nums[r])));
+                    do {
+                        l++;
+                    }
+                    while (l < r && nums[l] == nums[l - 1]);
+                }
+
+                else if (nums[i] + nums[l] + nums[r] > 0) r--;
+                else l++;
+            }
+        }
+        return result;
+    }
+}

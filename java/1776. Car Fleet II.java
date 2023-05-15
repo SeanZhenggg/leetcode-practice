5c2139250b7075bf767d27e493a8086f8c34fcae
Sean Zhengg <s410585038@gmail.com>
2023-05-16 00:06:11 +0800

    update 1776. Car Fleet II

--- java/1776. Car Fleet II.java
+++ java/1776. Car Fleet II.java
@@ -27,3 +27,43 @@ class Solution {
         return ans;
     }
 }
+
+// using priority queue solution
+public static double[] getCollisionTimes(int[][] cars) {
+    double[] ans = new double[cars.length];
+
+    for (int i = 0; i < ans.length; i++) ans[i] = -1.0;
+
+    PriorityQueue<Node> pq = new PriorityQueue<>((a, b) -> Double.compare(a.time, b.time));
+
+    TreeSet<Integer> ts = new TreeSet<>();
+    for (int i = 0; i < cars.length; i++) ts.add(i);
+
+    for(int i = 0; i < cars.length - 1; i++) {
+        if(cars[i][1] > cars[i + 1][1]) {
+            pq.offer(new Node(i, (double)(cars[i + 1][0] - cars[i][0]) / (cars[i][1] - cars[i+1][1])));
+        }
+    }
+
+    while(!pq.isEmpty()) {
+        Node top = pq.poll();
+
+        if(ans[top.node] != -1.0) continue;
+
+        ans[top.node] = top.time;
+
+        Object prev = ts.lower(top.node);
+
+        if(prev == null) continue;
+
+        int next = ts.higher(top.node);
+
+        if(cars[(int)prev][1] > cars[next][1]) {
+            pq.offer(new Node((int) prev, (double)(cars[next][0] - cars[(int)prev][0]) / (cars[(int)prev][1] - cars[next][1])));
+        }
+
+        ts.remove(top.node);
+    }
+
+    return ans;
+}

3f0a3a4eaf10b77349fcd52b38bdc15ba2d8b109
Sean Zhengg <s410585038@gmail.com>
2023-05-15 23:40:00 +0800

    1776. Car Fleet II in java

---
+++ java/1776. Car Fleet II.java
@@ -0,0 +1,28 @@
+// stack solution
+class Solution {
+    public static double[] getCollisionTimes(int[][] cars) {
+        double[] ans = new double[cars.length];
+
+        for (int i = 0; i < ans.length; i++) ans[i] = -1.0;
+
+        Stack<Integer> stack = new Stack<>();
+        for(int i = cars.length - 1; i >= 0; i--) {
+
+            while (!stack.empty() && (cars[i][1] <= cars[stack.peek()][1])) stack.pop();
+
+            while (!stack.empty()) {
+                double collisionTime = (double)(cars[stack.peek()][0] - cars[i][0]) / (cars[i][1] - cars[stack.peek()][1]);
+                if(collisionTime >= ans[stack.peek()] && ans[stack.peek()] != -1.0) {
+                    stack.pop();
+                } else {
+                    ans[i] = collisionTime;
+                    break;
+                }
+            }
+
+            stack.push(i);
+        }
+
+        return ans;
+    }
+}

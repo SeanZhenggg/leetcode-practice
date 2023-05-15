2f41766ba8e92f8dc332da6fb03587130338396f
Sean Zhengg <s410585038@gmail.com>
2023-05-15 23:40:21 +0800

    update 1776. Car Fleet II

--- java/1776. Car Fleet II.java
+++ java/1776. Car Fleet II.java
@@ -6,6 +6,7 @@ class Solution {
         for (int i = 0; i < ans.length; i++) ans[i] = -1.0;
 
         Stack<Integer> stack = new Stack<>();
+
         for(int i = cars.length - 1; i >= 0; i--) {
 
             while (!stack.empty() && (cars[i][1] <= cars[stack.peek()][1])) stack.pop();

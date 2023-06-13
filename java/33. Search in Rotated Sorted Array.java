7e1a4041d7c2fcd241e4e864ee45ae29cf4885ed
Sean Zhengg <s410585038@gmail.com>
2023-06-14 00:41:44 +0800

    update 33. Search in Rotated Sorted Array

--- java/33. Search in Rotated Sorted Array.java
+++ java/33. Search in Rotated Sorted Array.java
@@ -60,3 +60,33 @@ class Solution {
     return -1;
   }
 }
+
+class Solution {
+
+  public static int search(int[] nums, int target) {
+    int left = 0, right = nums.length - 1;
+
+    while (left <= right) {
+      int mid = (left + right) / 2;
+
+      if (target < nums[mid]) {
+        if (target >= nums[left]) right = mid - 1;
+        else {
+          if (nums[mid] >= nums[left]) left = mid + 1;
+          else right = mid - 1;
+        }
+      }
+      else if (target > nums[mid]) {
+        if (target <= nums[right]) left = mid + 1;
+        else {
+          if (nums[mid] <= nums[right]) right = mid - 1;
+          else left = mid + 1;
+        }
+      } else {
+        return mid;
+      }
+    }
+
+    return -1;
+  }
+}

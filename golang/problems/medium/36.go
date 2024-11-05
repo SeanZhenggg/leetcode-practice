011e1e59103e314d88d5c09eecdffacaa2ef6658
seanZhenggg <s410585038@gmail.com>
2024-11-06 02:20:44 +0800

    36 sol 2 in go

--- golang/problems/medium/36.go
+++ golang/problems/medium/36.go
@@ -51,3 +51,47 @@ func getNumber(board [][]byte, i, j int) int {
 		return -1
 	}
 }
+
+func IsValidSudoku2(board [][]byte) bool {
+	var (
+		rowNumCount = make(map[int]int)
+		colNumCount = make(map[int]int)
+		boxNumCount = make(map[int]int)
+	)
+
+	for i := 0; i < len(board); i++ {
+		for j := 0; j < len(board[i]); j++ {
+			rowNum := getNumber(board, i, j)
+			if rowNum != -1 {
+				rowNumCount[rowNum]++
+			}
+			colNum := getNumber(board, j, i)
+			if colNum != -1 {
+				colNumCount[colNum]++
+			}
+			if rowNumCount[rowNum] > 1 || colNumCount[colNum] > 1 {
+				return false
+			}
+		}
+		rowNumCount = make(map[int]int)
+		colNumCount = make(map[int]int)
+	}
+
+	for i := 0; i < 9; i++ {
+		for j := 0; j < 3; j++ {
+			for k := 0; k < 3; k++ {
+				x, y := (i/3)*3+j, 3*(i%3)+k // y is different from solution 1
+				boxNum := getNumber(board, x, y)
+				if boxNum != -1 {
+					boxNumCount[boxNum]++
+				}
+				if boxNumCount[boxNum] > 1 {
+					return false
+				}
+			}
+		}
+		boxNumCount = make(map[int]int)
+	}
+
+	return true
+}

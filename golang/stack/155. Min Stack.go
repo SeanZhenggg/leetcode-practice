28385b2747645be6408d728415b889ce5eb56100
seanZhenggg <s410585038@gmail.com>
2025-01-29 18:18:31 +0800

    update 155 in go

--- golang/stack/155. Min Stack.go
+++ golang/stack/155. Min Stack.go
@@ -38,45 +38,6 @@ func (this *MinStack) GetMin() int {
 	return this.minSt[len(this.minSt)-1]
 }
 
-type MinStack2 struct {
-	st    []int
-	minSt []int
-}
-
-func Constructor2() MinStack {
-	return MinStack{
-		st:    make([]int, 0),
-		minSt: make([]int, 0),
-	}
-}
-
-func (this *MinStack) Push2(val int) {
-	this.st = append(this.st, val)
-
-	if len(this.minSt) == 0 {
-
-	} else {
-		if this.minSt[len(this.minSt)-1] >= val {
-			this.minSt = append(this.minSt, val)
-		} else {
-			this.minSt = append(this.minSt, this.minSt[len(this.minSt)-1])
-		}
-	}
-}
-
-func (this *MinStack) Pop2() {
-	this.st = this.st[:len(this.st)-1]
-	this.minSt = this.minSt[:len(this.minSt)-1]
-}
-
-func (this *MinStack) Top2() int {
-	return this.st[len(this.st)-1]
-}
-
-func (this *MinStack) GetMin2() int {
-	return this.minSt[len(this.minSt)-1]
-}
-
 /**
  * Your MinStack object will be instantiated and called as such:
  * obj := Constructor();

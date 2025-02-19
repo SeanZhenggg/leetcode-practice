c60e10ceaf5fb2393796e0f58ae58e12fd5e25d3
seanZhenggg <s410585038@gmail.com>
2025-02-20 01:37:01 +0800

    215 in go

--- golang/heapAndPq/215. Kth Largest Element in an Array.go
+++ golang/heapAndPq/215. Kth Largest Element in an Array.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"math/rand"
 	"sort"
+	"time"
 )
 
 // 1. min heap solution
@@ -104,6 +105,82 @@ func partition3(nums []int, l, r int) int {
 func findKthLargest4(nums []int, k int) int {
 	sort.Ints(nums)
 	return nums[len(nums)-k]
+}
+
+// 5. other's solution
+func findKthLargest5(nums []int, k int) int {
+	rand.New(rand.NewSource(time.Now().UnixNano())) // https://gabrieleromanato.name/go-rand-seed-has-been-deprecated-a-solution
+	index := quickSelect(nums, 0, len(nums)-1, k)
+	return nums[index]
+}
+
+func quickSelect(nums []int, left, right, k int) int {
+	if left == right {
+		return left
+	}
+
+	pivotIndex := rand.Intn(right-left+1) + left
+	pivot := nums[pivotIndex]
+
+	// 三路切分: lt 表示小於 pivot 的右邊界
+	//          gt 表示大於 pivot 的左邊界
+	lt, i, gt := left, left, right
+	for i <= gt {
+		if nums[i] > pivot {
+			nums[lt], nums[i] = nums[i], nums[lt]
+			lt++
+			i++
+		} else if nums[i] < pivot {
+			nums[i], nums[gt] = nums[gt], nums[i]
+			gt--
+		} else {
+			i++
+		}
+	}
+	if k-1 < lt {
+		return quickSelect(nums, left, lt-1, k)
+	}
+	if k-1 > gt {
+		return quickSelect(nums, gt+1, right, k)
+	}
+	return lt
+}
+
+// 6. other's solution
+func findKthLargest6(nums []int, k int) int {
+	n := len(nums)
+	r := rand.Intn(n)
+
+	nums[r], nums[n-1] = nums[n-1], nums[r]
+
+	pivot := nums[n-1]
+
+	l, r, u := 0, 0, n-2
+	for r <= u {
+		switch {
+		case nums[r] > pivot:
+			nums[l], nums[r] = nums[r], nums[l]
+			r++
+			l++
+		case nums[r] < pivot:
+			nums[r], nums[u] = nums[u], nums[r]
+			u--
+		default:
+			r++
+		}
+	}
+
+	nums[r], nums[n-1] = nums[n-1], nums[r]
+
+	if r < k-1 {
+		return findKthLargest(nums[r+1:], k-r-1)
+	}
+
+	if l > k-1 {
+		return findKthLargest(nums[:l], k)
+	}
+
+	return nums[k-1]
 }
 
 func Test_FindKthLargest() {

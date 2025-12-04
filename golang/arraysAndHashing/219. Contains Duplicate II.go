c1691612f17e9dcbda41df4d4eba0df133b79288
sean.zheng <s410585038@gmail.com>
2025-12-04 23:20:23 +0800

    219 in go

---
+++ golang/arraysAndHashing/219. Contains Duplicate II.go
@@ -0,0 +1,44 @@
+package arraysAndHashing
+
+import "github.com/emirpasic/gods/sets/hashset"
+
+func containsNearbyDuplicate(nums []int, k int) bool {
+	for i := 0; i < len(nums); i++ {
+		for j := i + 1; j <= i+k; j++ {
+			if j > len(nums)-1 {
+				break
+			}
+			if nums[i] == nums[j] {
+				return true
+			}
+		}
+	}
+	return false
+}
+
+func containsNearbyDuplicate2(nums []int, k int) bool {
+	m := make(map[int]int)
+	for i := 0; i < len(nums); i++ {
+		if v, ok := m[nums[i]]; ok {
+			if i-v <= k {
+				return true
+			}
+		}
+		m[nums[i]] = i
+	}
+	return false
+}
+
+func containsNearbyDuplicate3(nums []int, k int) bool {
+	s := hashset.New()
+	for i := 0; i < len(nums); i++ {
+		if s.Contains(nums[i]) {
+			return true
+		}
+		s.Add(nums[i])
+		if s.Size() > k {
+			s.Remove(nums[i-k])
+		}
+	}
+	return false
+}

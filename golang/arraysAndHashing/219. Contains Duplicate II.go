c9504295edbfa934d959b6d7a3608c54cc58957e
sean.zheng <s410585038@gmail.com>
2025-12-04 23:30:58 +0800

    219 in md

--- golang/arraysAndHashing/219. Contains Duplicate II.go
+++ golang/arraysAndHashing/219. Contains Duplicate II.go
@@ -4,10 +4,7 @@ import "github.com/emirpasic/gods/sets/hashset"
 
 func containsNearbyDuplicate(nums []int, k int) bool {
 	for i := 0; i < len(nums); i++ {
-		for j := i + 1; j <= i+k; j++ {
-			if j > len(nums)-1 {
-				break
-			}
+		for j := i + 1; j < i+1+k && j < len(nums); j++ {
 			if nums[i] == nums[j] {
 				return true
 			}

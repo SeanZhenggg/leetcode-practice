752cf93d032b3625c7d860dc8b708479e70b2b5b
sean.zheng <s410585038@gmail.com>
2026-03-02 23:56:38 +0800

    424 updates

--- golang/slidingWindow/424. Longest Repeating Character Replacement_test.go
+++ golang/slidingWindow/424. Longest Repeating Character Replacement_test.go
@@ -22,72 +22,36 @@ var (
 	}
 )
 
-func TestCharacterReplacement(t *testing.T) {
+func TestCharacterReplacementFailed(t *testing.T) {
 	for _, c := range cases {
-		ans := characterReplacement(c.s, c.k)
+		ans := characterReplacementFailed(c.s, c.k)
 		if ans != c.expected {
 			t.Errorf("answer is %d, want %d", ans, c.expected)
 		}
 	}
 }
 
-func TestCharacterReplacement1(t *testing.T) {
+func TestCharacterReplacementBruteForce(t *testing.T) {
 	for _, c := range cases {
-		ans := characterReplacement1(c.s, c.k)
+		ans := characterReplacementBruteForce(c.s, c.k)
 		if ans != c.expected {
 			t.Errorf("answer is %d, want %d", ans, c.expected)
 		}
 	}
 }
 
-func TestCharacterReplacement2(t *testing.T) {
+func TestCharacterReplacementSlidingWindow(t *testing.T) {
 	for _, c := range cases {
-		ans := characterReplacement2(c.s, c.k)
+		ans := characterReplacementSlidingWindow(c.s, c.k)
 		if ans != c.expected {
 			t.Errorf("answer is %d, want %d", ans, c.expected)
 		}
 	}
 }
 
-func TestCharacterReplacement3(t *testing.T) {
+func TestCharacterReplacementSlidingWindowOptimal(t *testing.T) {
 	for _, c := range cases {
-		ans := characterReplacement3(c.s, c.k)
-		if ans != c.expected {
-			t.Errorf("answer is %d, want %d", ans, c.expected)
-		}
-	}
-}
-
-func TestCharacterReplacementReview(t *testing.T) {
-	for _, c := range cases {
-		ans := characterReplacementReview(c.s, c.k)
-		if ans != c.expected {
-			t.Errorf("answer is %d, want %d", ans, c.expected)
-		}
-	}
-}
-
-func TestCharacterReplacementReview2(t *testing.T) {
-	for _, c := range cases {
-		ans := characterReplacementReview2(c.s, c.k)
-		if ans != c.expected {
-			t.Errorf("answer is %d, want %d", ans, c.expected)
-		}
-	}
-}
-
-func TestCharacterReplacementReview3(t *testing.T) {
-	for _, c := range cases {
-		ans := characterReplacementReview3(c.s, c.k)
-		if ans != c.expected {
-			t.Errorf("answer is %d, want %d", ans, c.expected)
-		}
-	}
-}
-
-func TestCharacterReplacementReview5(t *testing.T) {
-	for _, c := range cases {
-		ans := characterReplacementReview5(c.s, c.k)
+		ans := characterReplacementSlidingWindowOptimal(c.s, c.k)
 		if ans != c.expected {
 			t.Errorf("answer is %d, want %d", ans, c.expected)
 		}

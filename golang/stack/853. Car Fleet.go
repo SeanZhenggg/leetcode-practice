73c0c5c45559be8dba9c37b14585a95db8662872
sean.zheng <s410585038@gmail.com>
2026-04-09 22:22:14 +0800

    853 updates

--- golang/stack/853. Car Fleet.go
+++ golang/stack/853. Car Fleet.go
@@ -1,7 +1,6 @@
 package stack
 
 import (
-	"log"
 	"slices"
 )
 
@@ -34,40 +33,54 @@ func carFleet(target int, position []int, speed []int) int {
 	return len(st)
 }
 
-func Test_CarFleet() {
-	target1 := 12
-	pos1 := []int{10, 8, 0, 5, 3}
-	speed1 := []int{2, 4, 1, 1, 3}
-	ans1 := carFleet(target1, pos1, speed1)
-	log.Printf("ans1: %v", ans1)
-	target2 := 10
-	pos2 := []int{3}
-	speed2 := []int{3}
-	ans2 := carFleet(target2, pos2, speed2)
-	log.Printf("ans2: %v", ans2)
-	target3 := 100
-	pos3 := []int{0, 2, 4}
-	speed3 := []int{4, 2, 1}
-	ans3 := carFleet(target3, pos3, speed3)
-	log.Printf("ans3: %v", ans3)
-	target4 := 10
-	pos4 := []int{6, 8}
-	speed4 := []int{3, 2}
-	ans4 := carFleet(target4, pos4, speed4)
-	log.Printf("ans4: %v", ans4)
-	target5 := 10
-	pos5 := []int{8, 3, 7, 4, 6, 5}
-	speed5 := []int{4, 4, 4, 4, 4, 4}
-	ans5 := carFleet(target5, pos5, speed5)
-	log.Printf("ans5: %v", ans5)
-	target6 := 10
-	pos6 := []int{2, 4}
-	speed6 := []int{3, 2}
-	ans6 := carFleet(target6, pos6, speed6)
-	log.Printf("ans6: %v", ans6)
-	target7 := 31
-	pos7 := []int{5, 26, 18, 25, 29, 21, 22, 12, 19, 6}
-	speed7 := []int{7, 6, 6, 4, 3, 4, 9, 7, 6, 4}
-	ans7 := carFleet(target7, pos7, speed7)
-	log.Printf("ans7: %v", ans7)
+func carFleet2(target int, position []int, speed []int) int {
+	// 大到小排，看看距離遠的能不能追上前面的人
+	m := make(map[int]int)
+	for i, v := range position {
+		m[v] = speed[i]
+	}
+
+	slices.SortFunc(position, func(a, b int) int {
+		return a - b
+	})
+	speed = make([]int, 0, len(position))
+	for _, v := range position {
+		speed = append(speed, m[v])
+	}
+
+	timeStack := new(Stack[float64])
+	for i := range position {
+		ti := float64(target-position[i]) / float64(speed[i])
+		for timeStack.Len() > 0 && timeStack.Top() <= ti {
+			timeStack.Pop()
+		}
+		timeStack.Push(ti)
+	}
+
+	return timeStack.Len()
+}
+
+func carFleetForIteration(target int, position []int, speed []int) int {
+	// 大到小排，看看距離遠的能不能追上前面的人
+	var posWithSpeed = make([][2]int, 0, len(position))
+
+	for i, v := range position {
+		posWithSpeed = append(posWithSpeed, [2]int{v, speed[i]})
+	}
+
+	var curCostTime float64
+	var fleetCount int
+	slices.SortFunc(posWithSpeed, func(a, b [2]int) int {
+		return b[0] - a[0]
+	})
+
+	for _, item := range posWithSpeed {
+		nextCostTime := float64(target-item[0]) / float64(item[1])
+		if curCostTime == 0 || curCostTime < nextCostTime {
+			curCostTime = nextCostTime
+			fleetCount += 1
+		}
+	}
+
+	return fleetCount
 }
